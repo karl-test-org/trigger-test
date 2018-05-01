74dbcf6b1eeb7854b86897b2cea7e76c0547c12c
Karl Hellstern <khellstern@google.com>
2018-05-01 15:36:06 -0700

    Removed a bunch of print lines

--- quickstart.sh
+++ quickstart.sh
@@ -1,9 +1,2 @@
 #!/bin/sh
 echo "Hello, world! The time is $(date)."
-echo "Second line"
-echo "Third line"
-echo "Fourth line"
-echo "Does this work"
-echo "Trying again..."
-echo "hello world"
-echo "hello world"
